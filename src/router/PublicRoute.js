94b498544a83f713f7177747e9bed28c6b392f97
minhbui1510 <buihaiminh.2k@gmail.com>
2025-03-18 11:15:37 +0700

    first commit

--- src/router/PublicRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-
-const PublicRoute = () => {
-    const isAuthenticated = true;
-    return isAuthenticated ? <Navigate to="/" /> : <Outlet/>;
-};
-
-export default PublicRoute;
